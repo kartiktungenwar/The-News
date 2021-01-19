72002c48ecdca0fa6c90cb878738113710214197
charlizesmithweb <charlizesmithweb@gmail.com>
2021-01-19 18:50:10 +0530

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 app.controller('customersCtrl', function($scope, $http) {
   $http({
     method: "GET",
-    url: "https://newsapi.org/v2/top-headlines?country=in&apiKey=952da71b07954070b1fd98d174b01d11",
+    url: "http://newsapi.org/v2/top-headlines?country=in&apiKey=952da71b07954070b1fd98d174b01d11",
    }).then(function (response) {
       $scope.myData = response.data.articles;
       console.log(response)
@@ -11,7 +11,7 @@ app.controller('customersCtrl', function($scope, $http) {
 
   $http({
     method: "GET",
-    url: "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=952da71b07954070b1fd98d174b01d11",
+    url: "http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=952da71b07954070b1fd98d174b01d11",
    }).then(function (response) {
       $scope.myBusiness = response.data.articles;
       console.log(response)
@@ -19,7 +19,7 @@ app.controller('customersCtrl', function($scope, $http) {
 
   $http({
     method: "GET",
-    url: "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=952da71b07954070b1fd98d174b01d11",
+    url: "http://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=952da71b07954070b1fd98d174b01d11",
    }).then(function (response) {
       $scope.myEntertainment = response.data.articles;
       console.log(response)
@@ -27,7 +27,7 @@ app.controller('customersCtrl', function($scope, $http) {
 
   $http({
     method: "GET",
-    url: "https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=952da71b07954070b1fd98d174b01d11",
+    url: "http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=952da71b07954070b1fd98d174b01d11",
    }).then(function (response) {
       $scope.myHealth = response.data.articles;
       console.log(response)
@@ -35,7 +35,7 @@ app.controller('customersCtrl', function($scope, $http) {
 
   $http({
     method: "GET",
-    url: "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=952da71b07954070b1fd98d174b01d11",
+    url: "http://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=952da71b07954070b1fd98d174b01d11",
    }).then(function (response) {
       $scope.mySports = response.data.articles;
       console.log(response)
@@ -43,7 +43,7 @@ app.controller('customersCtrl', function($scope, $http) {
 
   $http({
     method: "GET",
-    url: "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=952da71b07954070b1fd98d174b01d11",
+    url: "http://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=952da71b07954070b1fd98d174b01d11",
    }).then(function (response) {
       $scope.myTechnology = response.data.articles;
       console.log(response)
